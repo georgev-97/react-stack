94d54ac3a81e114df981515fd3b178a16969c663
georgev97 <georgeandrei.varga@gmail.com>
2023-02-22 20:03:27 +0100

    feat: add function hello

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
+import { Hello } from "./Hello";
 
 export class App extends React.Component{
     render(){
-        return <div></div>
+        return <Hello/>
     }
 }
