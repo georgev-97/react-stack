6ed6e134fdd846f62351da122e652ac05e85a059
georgev97 <georgeandrei.varga@gmail.com>
2023-02-22 19:58:22 +0100

    feat: add sumH2 function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 
+const sumH2 = (a, b) =><h2>{a+b}</h2>
+
 export class App extends React.Component{
     render(){
-        return <div></div>
+        return sumH2(199,1);
     }
 }
