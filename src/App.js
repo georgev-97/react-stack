bf793ebab46002d33c97cc25f37b17fa9800b1cb
georgev97 <georgeandrei.varga@gmail.com>
2023-02-23 16:55:49 +0100

    feat: add defaultProps

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Welcome } from "./Welcome";
 export class App extends React.Component{
     render(){
         return <div>
-            <Welcome name="Francesco"/>
+            <Welcome/>
         </div>
     }
 }
