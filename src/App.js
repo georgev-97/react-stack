1aace69d99a5fa8e2ffe248218df5b8ef47204a7
georgev97 <georgeandrei.varga@gmail.com>
2023-02-23 17:00:13 +0100

    feat: Add strong tag to name

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Welcome } from "./Welcome";
 
-export class App extends React.Component{
-    render(){
-        return <div>
-            <Welcome name="Maria" age={26}/>
-        </div>
-    }
+export class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Welcome name={<strong>Maria</strong>} age={26} />
+      </div>
+    );
+  }
 }
