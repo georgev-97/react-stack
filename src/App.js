e3d546027c92cd3de4aa825a2399d99f18d0083a
georgev97 <georgeandrei.varga@gmail.com>
2023-03-08 13:19:27 +0100

    feat: add john

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export class App extends React.Component {
   render() {
     return (
       <div>
-        <Welcome name={<strong>Maria</strong>} age={62} />
+        <Welcome name="John" age={62} />
       </div>
     );
   }
