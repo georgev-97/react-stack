fc90328328e4f50dbd0876478366f159fb60d0cd
georgev97 <georgeandrei.varga@gmail.com>
2023-03-08 13:43:40 +0100

    feat: componentdidmount

--- src/Counter.js
+++ src/Counter.js
@@ -5,13 +5,16 @@ class Counter extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0,
+      count: this.props.count,
     };
+  }
+
+  componentDidMount() {
     this._interval = setInterval(() => {
       this.setState((prevState) => ({
         count: prevState.count + this.props.amount,
       }));
-    },this.props.interval);
+    }, this.props.interval);
   }
 
   componentWillUnmount() {
@@ -19,13 +22,13 @@ class Counter extends React.Component {
   }
 
   render() {
-    return <CounterDisplay count={this.state.count}/>
+    return <CounterDisplay count={this.state.count} />;
   }
 }
 
 Counter.defaultProps = {
-    count: 0,
-    interval: 1000,
-    amount: 1
-}
+  count: 0,
+  interval: 1000,
+  amount: 1,
+};
 export default Counter;
