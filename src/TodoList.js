37450c116ba70a7357d74fdfeee2eeecf4bd7125
georgev97 <georgeandrei.varga@gmail.com>
2023-03-14 12:02:58 +0100

    feat: add reset

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,7 +1,10 @@
 /**Create a TodoList component that renders a ul tag with a li tag for each item contained in the items state variable.
  * The items state variable should be an array of strings. The TodoList component should also contain an input tag and a button.
  * When the button is clicked,
- * the event handler should add the value of the input tag to the items array. */
+ * the event handler should add the value of the input tag to the items array.
+ * Modify the TodoList component so that the input clears every time a Todo is added to the items array.
+ * 
+ * Modify the TodoList by adding a "reset" button that clears the items array when clicked. */
 
 import React from "react";
 
@@ -26,6 +29,13 @@ export class TodoList extends React.Component {
     })
   }
 
+  reset = () =>{
+    this.setState({
+        items: [],
+        toAdd: ""
+    })
+  }
+
   render() {
     return (
       <div>
@@ -36,6 +46,7 @@ export class TodoList extends React.Component {
         </ul>
         <input type="text" name="item" onChange={this.handleChange} value={this.state.toAdd}/>
         <button onClick={this.addItem}>Add</button>
+        <button onClick={this.reset}>Reset</button>
       </div>
     );
   }
