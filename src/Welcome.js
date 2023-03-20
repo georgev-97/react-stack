933204818029eb726f817f075fa4c4c2ab30d8bd
georgev97 <georgeandrei.varga@gmail.com>
2023-03-20 11:11:07 +0100

    feat: switch to function comp

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,17 +1,26 @@
-import React from "react";
+// import React from "react";
 import { Age } from "./Age";
 
-export class Welcome extends React.Component{
-    render(){
-        return(
-            <div>
-                <p>Welcome, {this.props.name}!</p>
-                <Age age={this.props.age}/>
-            </div>
-        )
-    }
-}
+// export class Welcome extends React.Component{
+//     render(){
+//         return(
+//             <div>
+//                 <p>Welcome, {this.props.name}!</p>
+//                 <Age age={this.props.age}/>
+//             </div>
+//         )
+//     }
+// }
 
-Welcome.defaultProps = {
-    name: "George"
+// Welcome.defaultProps = {
+//     name: "George"
+// }
+
+export function Welcome({name, age}){
+    return(
+        <div>
+            <p>Welcome, {name}!</p>
+            <Age age={age}/>
+        </div>
+    )
 }
