8b7213f13a9444ca808fe1d3732cc877804c2ba3
georgev97 <georgeandrei.varga@gmail.com>
2023-03-20 09:57:34 +0100

    feat:add style to welcome

---
+++ src/Welcome.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Welcome extends Component {
+  render() {
+    return (
+      <div className="welcome">
+        <h1>Hello world</h1>
+      </div>
+    )
+  }
+}
+
+export default Welcome
