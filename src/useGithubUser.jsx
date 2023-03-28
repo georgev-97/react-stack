484ae6636df2dca6230d740cfbf1c6166b2a29b4
georgev97 <georgeandrei.varga@gmail.com>
2023-03-28 09:21:38 +0200

    feat: add fetchgithubuser

--- src/useGithubUser.jsx
+++ src/useGithubUser.jsx
@@ -1,20 +1,23 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export function useGithubUser(username) {
   const [user, setUser] = useState(null);
   const [error, setError] = useState("");
+  const fetchUserData = useCallback(() => {
+      fetch(`https://api.github.com/users/${username}`)
+        .then((response) => {
+          if (response.status !== 200) {
+            setError("User not found");
+            throw new Error("User not found");
+          } else {
+            return response.json();
+          }
+        })
+        .then((user) => setUser(user))
+        .catch((error) => setError(error.message));
+  },[username])
   useEffect(() => {
-    fetch(`https://api.github.com/users/${username}`)
-      .then((response) => {
-        if (response.status !== 200) {
-          setError("User not found");
-          throw new Error("User not found");
-        } else {
-          return response.json();
-        }
-      })
-      .then((user) => setUser(user))
-      .catch((error) => setError(error.message));
-  }, [username]);
-  return [user, error];
+    fetchUserData();
+  }, [username, fetchUserData]);
+  return [user, error, fetchUserData];
 }
