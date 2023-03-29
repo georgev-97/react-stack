530725cfbef84a5d7154cceea827fa8aa6e2f162
georgev97 <georgeandrei.varga@gmail.com>
2023-03-29 09:35:28 +0200

    feat: add route counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import {Counter}  from "./Counter";
+import { Link, Route, Routes } from "react-router-dom";
+import { Counter } from "./Counter";
 
-export class App extends React.Component{
-    render(){
-        return <div>
-            <Counter initialValue={0} />
-        </div>
-    }
+export class App extends React.Component {
+  render() {
+    return (
+      <Routes>
+        <Route path="/" element={<Link to="/counter">Counter</Link>}/>
+        <Route path="/counter" element={<Counter initialValue={0} />} />
+      </Routes>
+    );
+  }
 }
