fc0dc96131b704199eda8e54b1a5bfe8f27299d6
georgev97 <georgeandrei.varga@gmail.com>
2023-03-29 10:37:15 +0200

    feat: add 404 route

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,12 @@ export class App extends React.Component {
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/login">Login</Link>
-
+        <Link to="/notfound">404</Link>
         <Routes>
           <Route path="/" element={<h1>Home</h1>} />
           <Route path="/about" element={<h1>About</h1>} />
           <Route path="/login" element={<h1>Login</h1>} />
+          <Route path="*" element={<h1>404</h1>} />
         </Routes>
       </div>
     );
