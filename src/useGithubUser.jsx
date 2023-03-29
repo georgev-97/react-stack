41914d7eb69f4df46e1eed621751a20c1eda7d7e
georgev97 <georgeandrei.varga@gmail.com>
2023-03-29 11:15:33 +0200

    feat: switch from useEffect to useSWR

--- src/useGithubUser.jsx
+++ src/useGithubUser.jsx
@@ -1,20 +1,7 @@
-import { useEffect, useState } from "react";
+import useSWR from "swr";
+const fetcher = (url) => fetch(url).then(result => result.json())
 
 export function useGithubUser(username) {
-  const [user, setUser] = useState(null);
-  const [error, setError] = useState("");
-  useEffect(() => {
-    fetch(`https://api.github.com/users/${username}`)
-      .then((response) => {
-        if (response.status !== 200) {
-          setError("User not found");
-          throw new Error("User not found");
-        } else {
-          return response.json();
-        }
-      })
-      .then((user) => setUser(user))
-      .catch((error) => setError(error.message));
-  }, [username]);
-  return [user, error];
+  const {data,error} = useSWR(`https://api.github.com/users/${username}`, fetcher);
+  return [data,error];
 }
